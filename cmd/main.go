11aee1ae1389b2a27fd95a96c70745acf587ec43
José Yáñez <joseyanez2298@gmail.com>
2026-07-04 19:30:10 -0500

    products(view): generating example list page

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joseyanez/dummies-app/internal/products/handlers"
 )
 
 func main() {
@@ -12,6 +13,10 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
+
+	productHandler := handlers.NewProductHandler()
+
+	r.Get("/products", productHandler.GetProducts)
 
 	http.ListenAndServe(":8080", r)
 }
--- internal/products/handlers/products.go
+++ internal/products/handlers/products.go
@@ -1,7 +1,17 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/joseyanez/dummies-app/internal/products/views/pages"
+)
+
 type ProductHandler struct{}
 
 func NewProductHandler() *ProductHandler {
 	return &ProductHandler{}
+}
+
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	pages.ListProductsPage().Render(r.Context(), w)
 }

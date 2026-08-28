8d6b44a6d99dd281deb5534931ae49cd01cc652a
José Yáñez <joseyanez2298@gmail.com>
2026-08-27 20:20:56 -0500

    try: SaveImage new function created

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,14 +2,67 @@ package storage
 
 import (
 	"fmt"
+	imagePkg "image"
 	"io"
 	"log"
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/google/uuid"
 )
+
+var widths = []int{400, 800, 1200}
+
+func NewSaveImages(images []*multipart.FileHeader) (filenames, imageErrors []string, generalError error) {
+	uploadDir := "web/static/images/"
+	var wgImages sync.WaitGroup
+	wgImages.Add(len(images))
+
+	imageErrorsChan := make(chan string, len(images))
+
+	for _, image := range images {
+		imageFile, err := image.Open()
+		if err != nil {
+			imageErrors = append(imageErrors, fmt.Sprintf("Error al abrir la imagen: %s", image.Filename))
+			log.Println("Failed to open image: ", err)
+		}
+
+		go func(imageFile multipart.File, filename string) {
+			defer wgImages.Done()
+			defer imageFile.Close()
+			// Validar imagen: (formato permitido y dimensiones máximas)
+			imgConfig, format, err := imagePkg.DecodeConfig(imageFile)
+			if err != nil {
+				imageErrorsChan <- fmt.Sprintf("Error al decodificar la imagen %s", filename)
+				log.Println("Error while image decode: ", err)
+				return
+			}
+			switch format {
+			case "jpeg", "png", "webp":
+				// Imagen válida
+			default:
+				imageErrorsChan <- fmt.Sprintf("Formato de imagen %s no válido", filename)
+				return
+			}
+			// Obtener el ancho de la imagen
+			imageWidth := imgConfig.Width
+
+			// Determinar variantes de la imagen necesarias
+			for _, targetWidth := range widths {
+				if targetWidth < imageWidth {
+					// generar nueva imagen
+				}
+			}
+			// Redimensionar manteniendo proporción
+			// Codificar como WebP
+			// Guardar variantes en disco
+		}(imageFile, image.Filename)
+	}
+
+	wgImages.Wait()
+}
 
 func SaveImages(images []*multipart.FileHeader) (filenames, errors []string) {
 	uploadDir := "web/static/images/"
